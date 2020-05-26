6897eda4f9ebd306d4c3566df661c33738cc291d
sid <sidgan22@gmail.com>
2020-05-26 14:49:24 +0530

    Tasks bug fix

--- src/pages/TaskMan.js
+++ src/pages/TaskMan.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Header from "../components/Header";
 import { db, auth } from "../services/firebase";
 import "../taskman.css";
-import TodoItem from "../components/TodoItem";
+// import TodoItem from "../components/TodoItem";
 class TaskMan extends Component {
     constructor() {
         super();
@@ -13,16 +13,18 @@ class TaskMan extends Component {
             assby: '',
             assto: '',
             notes: [],
-            listoft:[],
-            listofc:[],
-            listtask:[],
-            isAdmin:false,
-            loadingNotes:true,
+            listoft: [],
+            listofc: [],
+            listtask: [],
+            isAdmin: false,
+            loadingNotes: true,
+            formattedTasks: []
+
         }
 
         this.createNote = this.createNote.bind(this);
 
-        
+
 
     }
     onChangeHandler(evt, key) {
@@ -31,7 +33,7 @@ class TaskMan extends Component {
         });
     }
 
-    
+
 
 
 
@@ -40,8 +42,8 @@ class TaskMan extends Component {
         if (this.state.note !== '') {
             // var x = await this.formatTime();
             console.log("Craet");
-            db.ref('channels/'+this.state.assto+'/tasks').push({
-                note: "New note",
+            db.ref('channels/' + this.state.assto + '/tasks').push({
+                note: this.state.note,
                 assby: auth().currentUser.displayName,
                 deadline: this.state.datecomp,
                 creDate: this.state.dateass
@@ -62,61 +64,61 @@ class TaskMan extends Component {
         this.checkadmin();
 
         const d = Date.now().toString();
-        this.setState({dateass: d.toString()});
+        this.setState({ dateass: d.toString() });
 
     }
-    async checkadmin() {
-       
+    checkadmin() {
+
         const userC = db.ref().child('admin/aid/')
-        var y=false;
+        var y = false;
         userC.transaction(function (current_value) {
             console.log("Check admin " + current_value);
             var x = current_value || "";
             if (x.includes(auth().currentUser.uid))
-                y=true;
+                y = true;
             return current_value;
         });
-        if(y)
-            this.setState({isAdmin:true});
-        var str="";
-        var tref = db.ref().child("users/" + auth().currentUser.uid + "/tid");
-        tref.on("value", snapshot => {
-            str += snapshot.val();
-            console.log("Str is : " + str);
-            this.state.listoft.splice(0, this.state.listoft.length);
-            this.state.listoft.push(str.split(" "));
-            console.log(this.state.listoft);
-        });
+        if (y) {
+            this.setState({ isAdmin: true });
 
-        this.createNotes();
+            console.log("admin");
+            this.createNotes();
+        }
+        else {
+
+            console.log("Not admin");
+            this.createNotes();
+        }
+
+
     }
-    
 
     async createNotes() {
+
         var uidadmin = auth().currentUser.uid;
         var str = "";
         this.setState({ loadingNotes: true });
-            try {
-                db.ref('users/' + uidadmin + "/cid").on("value", snapshot => {
-                    str += snapshot.val();
-                    console.log("Str is : " + str);
-                    this.state.listofc.splice(0, this.state.listofc.length);
-                    this.state.listofc.push(str.split(" "));
-                    // listofcs=str;
-                    console.log(this.state.listofc);
-                });
-                // this.setState({ listofc });
+        try {
+            db.ref('users/' + uidadmin + "/cid").on("value", snapshot => {
+                str += snapshot.val();
+                console.log("Str is : " + str);
+                this.state.listofc.splice(0, this.state.listofc.length);
+                this.state.listofc.push(str.split(" "));
+                // listofcs=str;
+                console.log(this.state.listofc);
+            });
+            // this.setState({ listofc });
 
-            }
-            catch (error) {
-                console.log(error);
-            }
+        }
+        catch (error) {
+            console.log(error);
+        }
         try {
 
-            await db.ref('channels').on("value", snapshot => {
+            db.ref('channels').on("value", snapshot => {
 
                 let listtaskss = [];
-                
+
                 let x = "";
                 snapshot.forEach((snap) => {
 
@@ -131,7 +133,7 @@ class TaskMan extends Component {
 
                             console.log("Includes");
                             listtaskss.push(snap.val());
-                            
+
 
                             console.log(listtaskss);
                         }
@@ -140,42 +142,76 @@ class TaskMan extends Component {
                     // console.log(snap.val());
                 });
 
+                this.state.listtask = listtaskss;
 
-                this.setState({ listtask:listtaskss });
+                console.log(this.state.listtask);
+                var aa = this.state.listtask;
+                var xx = aa[0]['tasks'];
+                console.log(xx);
+
+                const tasks = Object.values(xx);
+
+                tasks.forEach(task =>
+                    Object.entries(task).forEach(([key, value]) =>
+                        this.state.formattedTasks.push(value),
+                    )
+                );
+                console.log(this.state.formattedTasks[2]);
+                var assby = this.state.formattedTasks[0];
+
+                this.setState({ assby });
+                var dateass = Date(this.state.formattedTasks[1]).toString().substring(0,17);
+                this.setState({dateass});
+
+                var datecomp = this.state.formattedTasks[2].toString();
+
+                this.setState({ datecomp });
+                var note = this.state.formattedTasks[3];
+                this.setState({ note });
+                console.log("Ass by: " + this.state.assby);
+                console.log("Deadline: " + this.state.datecomp);
+                console.log("Date ass: " + this.state.dateass);
+                console.log("Note: " + this.state.note);
+
             });
 
             this.setState({ loadingNotes: false });
             this.renderChatss();
 
         } catch (error) {
-            this.setState({  loadingNotes: false });
+            this.setState({ loadingNotes: false });
         }
         this.renderChatss();
 
     }
 
     async renderChatss() {
+        console.log("Render CHats ");
+        for (var i = 0; i < this.state.listtask.length; i++) {
+            var cchannelid = this.state.listtask[i];//this.state.assto == null ? "1" : this.state.assto.toString();
+            try {
+                console.log(cchannelid);
+                this.setState({ loadingNotes: true });
 
+                db.ref("channels/" + cchannelid + "/tasks").on("value", snapshot => {
+                    let listoft = [];
 
-        var cchannelid = this.state.assto == null ? "1" : this.state.assto.toString();
-        try {
-            this.setState({ loadingNotes: true });
-            
-            db.ref("channels/" + cchannelid + "/task").on("value", snapshot => {
-                let listoft = [];
-
-                snapshot.forEach((snap) => {
-                    listoft.push(snap.val());
+                    snapshot.forEach((snap) => {
+                        listoft.push(snap.val());
+                    });
+                    listoft.sort(function (a, b) { return a.timestamp - b.timestamp })
+                    this.setState({ listoft });
+                    this.setState({ loadingNotes: false });
                 });
-                listoft.sort(function (a, b) { return a.timestamp - b.timestamp })
-                this.setState({ listoft });
-                this.setState({ loadingNotes: false });
-            });
-        }
-        catch (error) {
+                console.log("Rebderd");
+            }
+            catch (error) {
+
+
+                console.log("list" + this.state.listoft);
+            }
 
         }
-
 
     }
 
@@ -189,20 +225,20 @@ class TaskMan extends Component {
                 <br></br>
 
                 <br></br>
-                {!this.state.isAdmin?<div> <h3 className="text-capitalize text-center">Task Manager</h3>
-                <div className="card card-body my-3">
-                    <form>
-                        <input className="form-control form-control-lg" id="" type="text" onChange={(evt) => this.onChangeHandler(evt, 'note')} value={this.state.note} placeholder="Task..." />
-                        <input className="form-control form-control-lg mt-2" type="text" onChange={(evt) => this.onChangeHandler(evt, 'assto')} value={this.state.assto} placeholder="Assign to.." />
-                        <input type="date" placeholder="Deadline" onChange={(evt) => this.onChangeHandler(evt, 'datecomp')} value={this.state.datecomp}/>
+                {!this.state.isAdmin ? <div> <h3 className="text-capitalize text-center">Task Manager</h3>
+                    <div className="card card-body my-3">
+                        <form>
+                            <input className="form-control form-control-lg" id="" type="text" onChange={(evt) => this.onChangeHandler(evt, 'note')} value={this.state.note} placeholder="Task..." />
+                            <input className="form-control form-control-lg mt-2" type="text" onChange={(evt) => this.onChangeHandler(evt, 'assto')} value={this.state.assto} placeholder="Assign to.." />
+                            <input type="date" placeholder="Deadline" onChange={(evt) => this.onChangeHandler(evt, 'datecomp')} value={this.state.datecomp} />
 
 
-                        <button type="submit" onClick={this.createNote} className="btn btn-block btn-primary mt-3">
-                            Add task
+                            <button type="submit" onClick={this.createNote} className="btn btn-block btn-primary mt-3">
+                                Add task
                         </button>
 
-                    </form>
-                </div></div>:null}
+                        </form>
+                    </div></div> : null}
                 <br></br>
                 <div className="sticky-top">
 
@@ -214,10 +250,10 @@ class TaskMan extends Component {
                         <li className="list-group-item text-capitalize d-flex justify-content-between my-4">
                             <div className="col">
                                 <p>
-                                Content: Project Viva<br></br>
-                                Assigned by: Siddarth Ganesh<br></br>
-                                Assigned on: 05/21/2020<br></br>
-                                Deadline:   05/25/2020<br></br>
+                                    Content: {this.state.note}<br></br>
+                                Assigned by: {this.state.assby}<br></br>
+                                Assigned on: {this.state.dateass}<br></br>
+                                Deadline:   {this.state.datecomp}<br></br>
                                 </p>
                             </div>
 
